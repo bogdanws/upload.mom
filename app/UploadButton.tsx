cd7ce45f532429104e5eaa14b0754feeca5bd49a
Bogdan Andreiana <53015677+bogdanws@users.noreply.github.com>
2023-12-07 17:53:04 +0200

    Refactor UploadButton into its own component
    The upload button and related functionalities from Upload.tsx have been moved to a new component: UploadButton.tsx. This enhances the reusability of the upload button component and improves readability in the Upload.tsx file, allowing easier future modifications. The changes to Upload.tsx primarily involve removal of the upload button code, with the inclusion of the new component at the end.

---
+++ app/UploadButton.tsx
@@ -0,0 +1,42 @@
+import {useState} from "react";
+import {motion} from "framer-motion";
+
+export function UploadButton() {
+	const [isHovered, setIsHovered] = useState(false);
+
+	const handleMouseEnter = () => {
+		setIsHovered(true);
+	};
+
+	const handleMouseLeave = () => {
+		setIsHovered(false);
+	};
+
+	return (
+		<div className={"relative flex flex-col items-center justify-center"}>
+			<button
+				disabled={true}
+				className="bg-blue-500 hover:bg-blue-600 disabled:hover:bg-amber-500 text-white font-bold py-2 px-4 rounded transition duration-700 ease-in-out transform hover:-translate-y-0.5 hover:scale-105 disabled:hover:-translate-y-0 disabled:hover:scale-100 disabled:cursor-not-allowed"
+				onMouseOver={handleMouseEnter}
+				onMouseLeave={handleMouseLeave}
+			>
+				Upload
+			</button>
+			<motion.div
+				className={`noFilesNotification mt-2 text-sm text-white bg-neutral-500 p-2 rounded text-center`}
+				initial={{opacity: 0}}
+				animate={{
+					opacity: isHovered ? 1 : 0,
+					y: isHovered ? 0 : -3,
+					display: "block",
+					transitionEnd: {
+						display: isHovered ? "block" : "none",
+					}
+				}}
+				transition={{duration: 0.3, ease: "easeInOut"}}
+			>
+				No files selected
+			</motion.div>
+		</div>
+	);
+}
