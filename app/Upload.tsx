7a2048afe205f120c9c584acddee2d4404d64f73
Bogdan Andreiana <53015677+bogdanws@users.noreply.github.com>
2023-12-09 22:48:47 +0200

    Refactor Upload component to improve code readability
    The drag-and-drop related functions, state and JSX within the Upload component were moved to a new UploadContainer component.

--- app/Upload.tsx
+++ app/Upload.tsx
@@ -1,58 +1,20 @@
 import "./Upload.scss";
 import {PrimaryActionButton} from "@/components/PrimaryActionButton";
 import {UploadMethods} from "@/app/UploadMethods";
-import React, {useCallback, useState} from "react";
+import React, {useState} from "react";
+import {UploadContainer} from "@/app/UploadContainer";
 
 export function Upload({}) {
 	const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
-	const [isDragging, setIsDragging] = useState(false);
-
-	const dragOverHandler = useCallback((event: React.DragEvent) => {
-		event.preventDefault();
-	}, []);
-
-	const dragEnterHandler = useCallback(() => {
-		setIsDragging(true);
-	}, []);
-
-	const dragLeaveHandler = useCallback((event: React.DragEvent) => {
-		if (event.currentTarget.contains(event.relatedTarget as Node)) {
-			// If the mouse pointer is entering a child element, do not execute the rest of the code
-			return;
-		}
-		setIsDragging(false);
-	}, []);
-
-	const dropHandler = useCallback((event: React.DragEvent) => {
-		event.preventDefault();
-		setIsDragging(false);
-
-		if (event.dataTransfer.items) {
-			const files = Array.from(event.dataTransfer.items)
-				.filter((item) => item.kind === 'file')
-				.map((item) => item.getAsFile())
-				.filter((file): file is File => file !== null);
-			setUploadedFiles((prevFiles) => [...prevFiles, ...files]);
-		}
-	}, []);
 
 	return (
-		<div
-			className={`uploadOuterContainer p-1 ${isDragging ? 'bg-gray-800' : 'bg-neutral-800'} transition duration-300 rounded flex flex-col items-center justify-center relative`}
-			onDragOver={dragOverHandler}
-			onDrop={dropHandler}
-			onDragEnter={dragEnterHandler}
-			onDragLeave={dragLeaveHandler}
-		>
+		<UploadContainer setUploadedFiles={setUploadedFiles}>
 			{/*<div className="gradient" TODO: Hide overflow />*/}
-			<div
-				className={`uploadInnerContainer p-7 pt-4 border-2 ${isDragging ? 'border-blue-500' : 'border-neutral-500'} border-dotted rounded transition duration-300 flex flex-col items-center justify-center`}>
-				{uploadedFiles.length === 0 &&
-					<h1 className="mb-5 text-3xl text-neutral-100 font-bold text-center">Upload your files</h1>}
-				<UploadMethods uploadedFiles={uploadedFiles} setUploadedFiles={setUploadedFiles}/>
-				{uploadedFiles.length > 0 && <PrimaryActionButton text={"Upload"}/>}
-			</div>
-		</div>
+			{uploadedFiles.length === 0 &&
+				<h1 className="mb-5 text-3xl text-neutral-100 font-bold text-center">Upload your files</h1>}
+			<UploadMethods uploadedFiles={uploadedFiles} setUploadedFiles={setUploadedFiles}/>
+			{uploadedFiles.length > 0 && <PrimaryActionButton text={"Upload"}/>}
+		</UploadContainer>
 	);
 }
 
---
+++ app/UploadContainer.tsx
@@ -0,0 +1,47 @@
+import React, {useCallback, useState} from "react";
+
+export function UploadContainer({children, setUploadedFiles}: { children: React.ReactNode, setUploadedFiles: React.Dispatch<React.SetStateAction<File[]>> }) {
+	const [isDragging, setIsDragging] = useState(false);
+
+	const dragOverHandler = useCallback((event: React.DragEvent) => {
+		event.preventDefault();
+	}, []);
+
+	const dragEnterHandler = useCallback(() => {
+		setIsDragging(true);
+	}, []);
+
+	const dragLeaveHandler = useCallback((event: React.DragEvent) => {
+		if (event.currentTarget.contains(event.relatedTarget as Node)) {
+			// If the mouse pointer is entering a child element, do not execute the rest of the code
+			return;
+		}
+		setIsDragging(false);
+	}, []);
+
+	const dropHandler = useCallback((event: React.DragEvent) => {
+		event.preventDefault();
+		setIsDragging(false);
+
+		if (event.dataTransfer.items) {
+			const files = Array.from(event.dataTransfer.items)
+				.filter((item) => item.kind === 'file')
+				.map((item) => item.getAsFile())
+				.filter((file): file is File => file !== null);
+			setUploadedFiles((prevFiles) => [...prevFiles, ...files]);
+		}
+	}, []);
+
+	return <div
+		className={`uploadOuterContainer p-1 ${isDragging ? 'bg-gray-800' : 'bg-neutral-800'} transition duration-300 rounded flex flex-col items-center justify-center relative`}
+		onDragOver={dragOverHandler}
+		onDrop={dropHandler}
+		onDragEnter={dragEnterHandler}
+		onDragLeave={dragLeaveHandler}
+	>
+		<div
+			className={`uploadInnerContainer p-7 pt-4 border-2 ${isDragging ? 'border-blue-500' : 'border-neutral-500'} border-dotted rounded transition duration-300 flex flex-col items-center justify-center`}>
+			{children}
+		</div>
+	</div>;
+}
