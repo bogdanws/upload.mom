1d67437518c04bb35267d2ea0db3a7e69e4b69c0
Bogdan Andreiana <53015677+bogdanws@users.noreply.github.com>
2023-12-18 20:22:54 +0200

    Remove unnecessary files from .gitignore and refactor deleteFile function in page.tsx
    - Remove .idea/vcs.xml and .idea/watcherTasks.xml from .gitignore
    - Refactor deleteFile function in page.tsx to use dispatch instead of direct state manipulation

--- app/upload/view/page.tsx
+++ app/upload/view/page.tsx
@@ -9,7 +9,6 @@ import {useRouter} from "next/navigation";
 export default function ViewFiles() {
 	const router = useRouter();
 	const {state, dispatch} = useContext(UploadContext);
-	const deleteFile = (file: File) => dispatch({type: "REMOVE_FILE", file});
 
 	useEffect(() => {
 		if (state.length === 0) {
@@ -19,14 +18,14 @@ export default function ViewFiles() {
 
 	return <>
 		<div className="w-full flex flex-row items-center justify-between">
-			<p className="text-neutral-200 text-center">Click on a file to <span className="text-red-300">remove</span> it
+			<p className="text-neutral-200 text-center">Click on a file to <span className="text-red-300">remove</span> it:
 			</p>
 			<PrimaryActionButton url={"/upload"} text={"Add more"}/>
 		</div>
 		<ul
 			className={"w-full my-2 flex-1 overflow-y-scroll overflow-x-hidden relative"}>
 			<AnimatePresence mode="popLayout">
-				{state.map((file) => <File key={file.name} onClick={() => deleteFile(file)} file={file}/>)}
+				{state.map((file) => <File key={file.name} onClick={() => dispatch({type: "REMOVE_FILE", file})} file={file}/>)}
 			</AnimatePresence>
 		</ul>
 		<PrimaryActionButton text={"Upload"}/>
